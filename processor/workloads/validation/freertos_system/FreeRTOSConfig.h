38fb44c5de958f7d33c1f49284d1755e4e9cf437
Cheng <chiacheng.tsao@gmail.com>
2026-09-12 21:30:33 +0800

    Instrument FreeRTOS list-insert-end boundary

--- processor/workloads/validation/freertos_system/FreeRTOSConfig.h
+++ processor/workloads/validation/freertos_system/FreeRTOSConfig.h
@@ -79,6 +79,10 @@ void rp86QueueTraceStage( unsigned short stage, unsigned short detail );
     rp86QueueTraceStage( 12U, 0U )
 #define traceRETURN_vListInsert() \
     rp86QueueTraceStage( 13U, 0U )
+#define traceENTER_vListInsertEnd( pxList, pxNewListItem ) \
+    rp86QueueTraceStage( 14U, 0U )
+#define traceRETURN_vListInsertEnd() \
+    rp86QueueTraceStage( 15U, 0U )
 
 void rp86AssertFailed( unsigned short line );
 #define configASSERT( x ) do { if( ( x ) == 0 ) { rp86AssertFailed( ( unsigned short ) __LINE__ ); } } while( 0 )
