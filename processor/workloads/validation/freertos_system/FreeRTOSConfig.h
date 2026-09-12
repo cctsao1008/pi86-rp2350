e0a98ae16413b19c464da29a7c8bc01e33bdba14
Cheng <chiacheng.tsao@gmail.com>
2026-09-12 23:10:18 +0800

    Use volatile FreeRTOS list members for 16-bit Watcom

--- processor/workloads/validation/freertos_system/FreeRTOSConfig.h
+++ processor/workloads/validation/freertos_system/FreeRTOSConfig.h
@@ -7,7 +7,7 @@
 #define configUSE_PREEMPTION                    1
 #define configUSE_TIME_SLICING                  1
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
-#define configUSE_TICKLESS_IDLE                 0
+#define configTICKLESS_IDLE                     0
 #define configMAX_PRIORITIES                    4
 #define configMINIMAL_STACK_SIZE                96
 #define configMAX_TASK_NAME_LEN                 8
@@ -18,6 +18,7 @@
 #define configENABLE_BACKWARD_COMPATIBILITY     0
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 0
 #define configUSE_MINI_LIST_ITEM                1
+#define configLIST_VOLATILE                     volatile
 #define configUSE_NEWLIB_REENTRANT              0
 
 #define configUSE_TIMERS                        0
