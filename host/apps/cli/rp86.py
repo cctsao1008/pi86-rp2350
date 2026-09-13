2835b692db62fc8736686ffcb56310af54a083bd
Cheng <chiacheng.tsao@gmail.com>
2026-09-13 20:52:05 +0800

    Preserve existing Host CLI surface during restructure

--- host/apps/cli/rp86.py
+++ host/apps/cli/rp86.py
@@ -6,11 +6,8 @@ from __future__ import annotations
 import sys
 
 
-if "--public" in sys.argv or "--remote" in sys.argv:
-    if "--public" in sys.argv:
-        sys.argv.remove("--public")
-    if "--remote" in sys.argv:
-        sys.argv.remove("--remote")
+if "--public" in sys.argv:
+    sys.argv.remove("--public")
     from host.apps.remote.rp86_remote import main
 elif "--web" in sys.argv:
     sys.argv.remove("--web")
