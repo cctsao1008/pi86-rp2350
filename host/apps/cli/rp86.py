712c63c33af2d2d1884f09e5e94a044b182294d0
Cheng <chiacheng.tsao@gmail.com>
2026-09-13 18:24:51 +0800

    Preserve public CLI flag under new Host app layout

--- host/apps/cli/rp86.py
+++ host/apps/cli/rp86.py
@@ -6,8 +6,11 @@ from __future__ import annotations
 import sys
 
 
-if "--remote" in sys.argv:
-    sys.argv.remove("--remote")
+if "--public" in sys.argv or "--remote" in sys.argv:
+    if "--public" in sys.argv:
+        sys.argv.remove("--public")
+    if "--remote" in sys.argv:
+        sys.argv.remove("--remote")
     from host.apps.remote.rp86_remote import main
 elif "--web" in sys.argv:
     sys.argv.remove("--web")
