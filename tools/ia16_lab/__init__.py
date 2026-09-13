8f863c7fcf3a056bf41f6a7cf1fa69b57148d3bb
Cheng <chiacheng.tsao@gmail.com>
2026-09-13 21:21:20 +0800

    Export IA16 lab observability helpers

--- tools/ia16_lab/__init__.py
+++ tools/ia16_lab/__init__.py
@@ -1,12 +1,21 @@
 """Small binary-execution laboratory for RP86 IA16 workloads."""
 
 from .loader import LoadedWorkload, linear_address, load_p86w
-from .machine import IA16Machine, UnicornUnavailable
+from .machine import IA16Machine, RegisterState, UnicornUnavailable
+from .symbols import Symbol, SymbolTable
+from .trace import InstructionTrace, MemoryTrace, TraceRecorder, WatchRange
 
 __all__ = [
     "IA16Machine",
+    "InstructionTrace",
     "LoadedWorkload",
+    "MemoryTrace",
+    "RegisterState",
+    "Symbol",
+    "SymbolTable",
+    "TraceRecorder",
     "UnicornUnavailable",
+    "WatchRange",
     "linear_address",
     "load_p86w",
 ]
