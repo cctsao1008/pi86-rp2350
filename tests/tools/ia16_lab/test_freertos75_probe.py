8ce62a1478b5aff2312d6f92513fd9295c72d478
Cheng <chiacheng.tsao@gmail.com>
2026-09-13 22:22:17 +0800

    Test static-symbol visibility reporting

--- tests/tools/ia16_lab/test_freertos75_probe.py
+++ tests/tools/ia16_lab/test_freertos75_probe.py
@@ -1,6 +1,10 @@
 import unittest
 
-from tools.ia16_lab.freertos75_probe import matching_map_lines, matching_symbols
+from tools.ia16_lab.freertos75_probe import (
+    matching_map_lines,
+    matching_symbols,
+    missing_map_evidence,
+)
 from tools.ia16_lab.symbols import SymbolTable
 
 
@@ -25,7 +29,9 @@ class FreeRTOS75ProbeTests(unittest.TestCase):
                 "_prvAddCurrentTaskToDelayedList",
             },
         )
-        self.assertEqual(len(matching_map_lines(text)), 5)
+        lines = matching_map_lines(text)
+        self.assertEqual(len(lines), 5)
+        self.assertEqual(missing_map_evidence(table, lines), ())
 
     def test_raw_line_matching_preserves_unparsed_wlink_evidence(self) -> None:
         text = (
@@ -35,6 +41,18 @@ class FreeRTOS75ProbeTests(unittest.TestCase):
         self.assertEqual(
             matching_map_lines(text),
             ("not-a-symbol line mentioning xSuspendedTaskList",),
+        )
+
+    def test_missing_static_symbols_are_reported_without_inventing_addresses(self) -> None:
+        text = (
+            "  1000:075A       _uxListRemove\n"
+            "  1279:0022       _pxCurrentTCB\n"
+        )
+        table = SymbolTable.from_wlink_map(text)
+        lines = matching_map_lines(text)
+        self.assertEqual(
+            missing_map_evidence(table, lines),
+            ("xSuspendedTaskList", "pxReadyTasksLists"),
         )
 
 
